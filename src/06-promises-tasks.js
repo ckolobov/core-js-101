91d588f38b36b33d7918a37378b950af78fa783b
Constantine Kolobov <kolobovconstantine@gmail.com>
2022-01-05 19:15:01 +0300

    feat: solve 06-promises-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,16 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer !== 'boolean') {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+    if (isPositiveAnswer) {
+      resolve('Hooray!!! She said "Yes"!');
+    }
+    resolve('Oh no, she said "No".');
+  });
 }
 
 
@@ -48,8 +56,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +79,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,8 +100,27 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  const finished = (new Array(array.length)).fill(false);
+  return new Promise((resolve) => {
+    const results = [];
+    const getRes = () => {
+      if (finished.every((val) => val === true)) {
+        resolve(results.reduce(action));
+      }
+    };
+    for (let i = 0; i < array.length; i += 1) {
+      const promise = array[i];
+      promise.then((res) => {
+        results.push(res);
+        finished[i] = true;
+        getRes();
+      }).catch(() => {
+        finished[i] = true;
+        getRes();
+      });
+    }
+  });
 }
 
 module.exports = {
